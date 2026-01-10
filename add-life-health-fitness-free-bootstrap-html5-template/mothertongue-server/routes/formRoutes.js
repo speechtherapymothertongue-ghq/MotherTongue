714de02edbf505b51b22a9f45e26d7c3be5e7d4f
AADITYA207 <aaditya207cgupta@gmail.com>
2026-01-10 22:27:41 +0900

    removed_redundant

--- add-life-health-fitness-free-bootstrap-html5-template/mothertongue-server/routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/formRoutes.js
-const express = require('express');
-const router = express.Router();
-const { handleFormSubmit } = require('../controllers/submitController');
-
-router.post('/submit', handleFormSubmit);
-
-module.exports = router;
